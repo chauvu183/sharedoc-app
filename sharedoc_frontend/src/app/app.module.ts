86e76eec663af2849b1332feec3f303fe3ceab3d
chauvu183 <chau.vu@haw-hamburg.de>
2019-11-11 11:21:12 +0100

    initial angular

---
+++ sharedoc_frontend/src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PinboardComponent } from './pinboard/pinboard.component';
+import { RegisterComponent } from './register/register.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { UploadComponent } from './upload/upload.component';
+import { UploadDisplayComponent } from './upload-display/upload-display.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { DataService } from './data.service';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PinboardComponent,
+    RegisterComponent,
+    HomepageComponent,
+    NavComponent,
+    UploadComponent,
+    UploadDisplayComponent,
+    FooterComponent,
+    ProductListComponent,
+    ProductDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    HttpClientModule,
+    InMemoryWebApiModule.forRoot(DataService),
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
