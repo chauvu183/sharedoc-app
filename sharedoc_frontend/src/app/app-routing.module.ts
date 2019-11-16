dc69d1c130256d9a5ae33f060da179ae530ab9d1
chauvu183 <chau.vu@haw-hamburg.de>
2019-11-16 16:05:39 +0100

    update Upload and Menu bar

--- sharedoc_frontend/src/app/app-routing.module.ts
+++ sharedoc_frontend/src/app/app-routing.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
+// import {Routes} from '@angular/router';
+// import {HomepageComponent} from './homepage/homepage.component';
+// import {RegisterComponent} from './register/register.component';
+// import {PinboardComponent} from './pinboard/pinboard.component';
+// import {UploadComponent} from './upload/upload.component';
+// import {UploadDisplayComponent} from './upload-display/upload-display.component';
+
 // import { Routes, RouterModule } from '@angular/router';
+
 
 
 @NgModule({
