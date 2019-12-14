094fad15cd444c3013373e00289a733a7dd50ece
Christine Schuller <christineschuller@outlook.com>
2019-12-14 16:48:20 +0100

    add initial dummy data and render

--- sharedoc_frontend/src/app/upload-display/upload-display.component.ts
+++ sharedoc_frontend/src/app/upload-display/upload-display.component.ts
@@ -2,22 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-upload-display',
- /* template: 
-  <pdf-viewer [src] = "pdfSrc" [render-text] = "true" style = "display:block;"></pdf-viewer> */
   templateUrl: './upload-display.component.html',
-  styleUrls: ['./upload-display.component.css']
+  styleUrls: ['./upload-display.component.css'],
 })
 export class UploadDisplayComponent implements OnInit {
+  constructor() {}
 
-  pdfSrc = "src/app/assets/pdfs/PDF1";
+  ngOnInit() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  coursesOfStudy = ["AI"];
-  semesters = ["1.", "2.", "3.", "4.", "5.", "6."];
-  subjects = ["subject1", "subject2"];
-  professors = ["Heitsch", "Neitzke", "Wendholt", "Klauck"];
+  coursesOfStudy = ['AI'];
+  semesters = ['1.', '2.', '3.', '4.', '5.', '6.'];
+  subjects = ['subject1', 'subject2', 'subject3', 'subject4'];
+  professors = ['Heitsch', 'Neitzke', 'Wendholt', 'Klauck'];
+  documents = [
+    { title: 'A title', url: 'https://www.google.com' },
+    { title: 'Another title', url: 'https://www.youtube.com' },
+  ];
 }
