33a9675279cd35039b1a5d54fc36c1adbbd70a28
Christine Schuller <christineschuller@outlook.com>
2020-01-09 23:07:58 +0100

    pdfs funktionieren

--- sharedoc_frontend/src/app/upload-display/upload-display.component.ts
+++ sharedoc_frontend/src/app/upload-display/upload-display.component.ts
@@ -8,14 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class UploadDisplayComponent implements OnInit {
   constructor() {}
 
+  saveByteArray(reportName, byte) {
+    var blob = new Blob([byte], { type: 'application/pdf' });
+    var link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    var fileName = reportName;
+    // link.download = fileName;
+    link.click();
+  }
+
+  base64ToArrayBuffer(base64) {
+    var binaryString = window.atob(base64);
+    var binaryLen = binaryString.length;
+    var bytes = new Uint8Array(binaryLen);
+    for (var i = 0; i < binaryLen; i++) {
+      var ascii = binaryString.charCodeAt(i);
+      bytes[i] = ascii;
+    }
+    return bytes;
+  }
+
   todos = [];
 
   ngOnInit() {
     fetch('http://sharedoc.informatik.haw-hamburg.de/spring/documents')
       .then(response => response.json())
       .then(json => {
-        this.todos = json;
-        console.log(json);
+        this.todos = json.map(item => {
+          const { data } = item;
+          return { ...item, data: this.base64ToArrayBuffer(data) };
+        });
+        console.log(this.todos);
       });
   }
 
