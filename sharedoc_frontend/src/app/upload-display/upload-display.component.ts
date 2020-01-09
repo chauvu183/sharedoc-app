9fe65b1ca50b039e8860ad034e35c1bacd762eeb
local_admin <local_admin@ws-76-214.haw-1x.haw-hamburg.de>
2020-01-09 15:23:38 +0100

    upload-display-files-changed

--- sharedoc_frontend/src/app/upload-display/upload-display.component.ts
+++ sharedoc_frontend/src/app/upload-display/upload-display.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-upload-display',
   templateUrl: './upload-display.component.html',
@@ -7,17 +6,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadDisplayComponent implements OnInit {
   constructor() {}
-
-  dokumente = [];
-
+  todos = [];
   ngOnInit() {
-    fetch('localhost:8080/documents')
+    fetch('/documents')
       .then(response => response.json())
-      .then(json => (this.dokumente = json));
+      .then(json => (this.todos = json));
   }
-
   coursesOfStudy = ['AI'];
   semesters = ['1.', '2.', '3.', '4.', '5.', '6.'];
   subjects = ['subject1', 'subject2', 'subject3', 'subject4'];
   professors = ['Heitsch', 'Neitzke', 'Wendholt', 'Klauck'];
+  documents = [
+    { name: 'Alice in Wonderland', id: 'someid' },
+    { name: 'Avengers: Endgame', id: 'someotherid' },
+  ];
 }
